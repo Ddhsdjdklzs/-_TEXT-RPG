2cc9cb4b9c2f295654a9cd18f0a7797638939e11
DEBBB <102015957+Ddhsdjdklzs@users.noreply.github.com>
2024-03-15 12:12:12 +0900

    Text Rpg.js

--- Text Rpg.js
+++ Text Rpg.js
@@ -1,40 +1,8 @@
-//대응소스 (수정X)
-function onNotificationPosted(sbn, sm) {
-    var packageName = sbn.getPackageName();
-    if (!packageName.startsWith("com.kakao.tal")) return;
-    var actions = sbn.getNotification().actions;
-    if (actions == null) return;
-    var userId = sbn.getUser().hashCode();
-    for (var n = 0; n < actions.length; n++) {
-      var action = actions[n];
-      if (action.getRemoteInputs() == null) continue;
-      var bundle = sbn.getNotification().extras;
-      var realId = bundle.getParcelableArray("android.messages")[0].get("sender_person").getKey();
-      var msg = bundle.get("android.text").toString();
-      var sender = bundle.getString("android.title");
-      var room = bundle.getString("android.subText");
-      if (room == null) room = bundle.getString("android.summaryText");
-      var isGroupChat = room != null;
-      if (room == null) room = sender;
-      var replier = new com.xfl.msgbot.script.api.legacy.SessionCacheReplier(packageName, action, room, false, "");
-      var icon = bundle.getParcelableArray("android.messages")[0].get("sender_person").getIcon().getBitmap();
-      var image = bundle.getBundle("android.wearable.EXTENSIONS");
-      if (image != null) image = image.getParcelable("background");
-      var imageDB = new com.xfl.msgbot.script.api.legacy.ImageDB(icon, image);
-      com.xfl.msgbot.application.service.NotificationListener.Companion.setSession(packageName, room, action);
-      if (this.hasOwnProperty("responseFix")) {
-        responseFix(room, msg, sender, isGroupChat, replier, imageDB, packageName, userId != 0, realId);
-      }
-    }
-}
-
 const j = '!';
 
 const AS = '\u200d'.repeat(500);
 
 const FS = FileStream;
-
-//«(칭호없음)버터»
 
 const Path = '/sdcard/Life/', UserPath = '/sdcard/Life/UserListData.txt', ServerPath = '/sdcard/Life/ServerData.json';
 
@@ -658,21 +626,3 @@ function Make(Inventory, Amount, Ingredients, ItemName, ItemRank, ItemAmount){
 
 }
 
-/*
-
-[THE LIFE]
-
--가입
-
--광질
-
--가방
-
--곡괭이
-
--곡괭아 강화
-
--곡괭이 
-
--곡괭이 인첸트
-*/
