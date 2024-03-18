d353abb686c56e5f0308c7529c4a8a8b10c5891b
DEBBB <102015957+Ddhsdjdklzs@users.noreply.github.com>
2024-03-18 12:15:14 +0900

    모듈

---
+++ LifeModule.js
@@ -0,0 +1,405 @@
+//도감
+exports.Dogam = [
+
+];
+
+//나무
+exports.Woods = {
+
+    'Common' : [
+        
+        {'Name' : '참나무', 'Rank' : '일반'},
+    
+        {'Name' : '소나무', 'Rank' : '일반'},
+    
+        {'Name' : '자작나무', 'Rank' : '일반'},
+    
+        {'Name' : '가문비나무', 'Rank' : '일반'}
+
+    ],
+
+    'Rare' : [
+        
+        {'Name' : '짙은 참나무', 'Rank' : '희귀'},
+    
+        {'Name' : '정글나무', 'Rank' : '희귀'},
+    
+        {'Name' : '아카시아 나무', 'Rank' : '희귀'},
+    
+        {'Name' : '단풍나무', 'Rank' : '희귀'}
+
+    ],
+
+    'SuperRare' : [
+        
+        {'Name' : '야자수', 'Rank' : '초희귀'},
+    
+        {'Name' : '히코리 나무', 'Rank' : '초희귀'},
+    
+        {'Name' : '짙은 자작나무', 'Rank' : '초희귀'},
+    
+        {'Name' : '벚꽃나무', 'Rank' : '초희귀'}
+
+    ],
+
+    'Epic' : [
+        
+        {'Name' : '영혼나무', 'Rank' : '영웅'},
+    
+        {'Name' : '편백나무', 'Rank' : '영웅'},
+    
+        {'Name' : '고무나무', 'Rank' : '영웅'},
+    
+        {'Name' : '메이플 나무', 'Rank' : '영웅'}
+
+    ],
+
+    'Legendary' : [
+        
+        {'Name' : '고대 나무', 'Rank' : '전설'},
+    
+        {'Name' : '포플러', 'Rank' : '전설'},
+    
+        {'Name' : '오리나무', 'Rank' : '전설'}
+
+    ],
+
+    'OverPowered' : [
+    
+        {'Name' : '나무 화석', 'Rank' : '초월'},
+    
+        {'Name' : '번개 나무', 'Rank' : '초월'},
+    
+        {'Name' : '빛나는 나무조각', 'Rank' : '초월'}
+
+    ],
+
+    'Infinity' : [
+        
+        {'Name' : '화려한 나무', 'Rank' : '영원'},
+    
+        {'Name' : '무지개 나무', 'Rank' : '영원'}
+
+    ],
+
+    'Deathfull' : [
+        
+        {'Name' : '불타는 나무', 'Rank' : '불멸'},
+    
+        {'Name' : '푸른 나무', 'Rank' : '불멸'}
+
+    ] 
+
+}
+
+//광물
+exports.Minerals = {
+
+    'Common' : [
+        
+        {'Name' : '돌', 'Rank' : '일반'},
+    
+        {'Name' : '석탄', 'Rank' : '일반'},
+    
+        {'Name' : '구리', 'Rank' : '일반'},
+    
+        {'Name' : '흑연', 'Rank' : '일반'},
+    
+        {'Name' : '철', 'Rank' : '일반'},
+    
+        {'Name' : '석영', 'Rank' : '일반'},
+
+    ],
+
+    'Rare' : [
+
+        {'Name' : '망간', 'Rank' : '희귀'},
+
+        {'Name' : '자수정', 'Rank' : '희귀'},
+
+        {'Name' : '은', 'Rank' : '희귀'},
+
+        {'Name' : '흑철석', 'Rank' : '희귀'},
+
+        {'Name' : '백금', 'Rank' : '희귀'},
+
+    ],
+
+    'SuperRare' : [
+
+        {'Name' : '알루미늄', 'Rank' : '초희귀'},
+
+        {'Name' : '연옥', 'Rank' : '초희귀'},
+
+        {'Name' : '석고', 'Rank' : '초희귀'},
+
+        {'Name' : '리튬', 'Rank' : '초희귀'},
+
+        {'Name' : '흑요석', 'Rank' : '초희귀'},
+
+        {'Name' : '홍주석', 'Rank' : '초희귀'}
+
+    ],
+
+    'Epic' : [
+
+        {'Name' : '금', 'Rank' : '영웅'},
+
+        {'Name' : '우라늄', 'Rank' : '영웅'},
+
+        {'Name' : '에메랄드', 'Rank' : '영웅'},
+
+        {'Name' : '미스릴', 'Rank' : '영웅'},
+
+        {'Name' : '쿤자이트', 'Rank' : '영웅'},
+
+        {'Name' : '로들라이트', 'Rank' : '영웅'}
+
+    ],
+
+    'Legendary' : [
+
+        {'Name' : '루비', 'Rank' : '전설'},
+
+        {'Name' : '티타늄', 'Rank' : '전설'},
+
+        {'Name' : '다이아몬드', 'Rank' : '전설'},
+
+        {'Name' : '아메트린', 'Rank' : '전설'},
+
+        {'Name' : '오리하르콘', 'Rank' : '전설'},
+
+        {'Name' : '아다만티움', 'Rank' : '전설'}
+
+    ],
+
+    'OverPowered' : [
+
+        {'Name' : '지르콘', 'Rank' : '초월'},
+
+        {'Name' : '진주', 'Rank' : '초월'},
+
+        {'Name' : '머스그레이비트', 'Rank' : '초월'},
+
+        {'Name' : '파이어 오팔', 'Rank' : '초월'},
+
+        {'Name' : '레드 아다만티움', 'Rank' : '초월'},
+
+        {'Name' : '데만토이드가넷', 'Rank' : '초월'}
+
+    ],
+
+    'Infinity' : [
+
+        {'Name' : '흑진주', 'Rank' : '영원'},
+
+        {'Name' : '레인보우 다이아몬드', 'Rank' : '영원'},
+
+        {'Name' : '운석', 'Rank' : '영원'},
+
+        {'Name' : '녹아버린 돌', 'Rank' : '영원'},
+
+        {'Name' : '행성 파편', 'Rank' : '영원'},
+
+        {'Name' : '파이어 크리스탈', 'Rank' : '영원'}
+
+    ],
+
+    'Deathfull' : [
+
+        {'Name' : '반물질', 'Rank' : '불멸'},
+
+        {'Name' : '초전도체', 'Rank' : '불멸'},
+
+        {'Name' : '불의 조각', 'Rank' : '불멸'},
+
+        {'Name' : '세계수 조각', 'Rank' : '불멸'},
+
+        {'Name' : '고대의 서적', 'Rank' : '불멸'},
+
+        {'Name' : 'ERROR', 'Rank' : '불멸'}
+
+    ]
+
+}
+
+//연구
+exports.Laboratory = {
+
+    '1' : [
+        
+        {'Name' : '참나무 계단', 'Rank' : '일반', 'Amount' : 2, 'Maker' : [
+            
+            {'Name' : '참나무', 'Rank' : '일반', 'Amount' : 2},
+            
+            {'Name' : '소나무', 'Rank' : '일반', 'Amount' : 1},
+            
+            {'Name' : '돌', 'Rank' : '일반', 'Amount' : 1}
+        
+            ]
+        },
+
+        {'Name' : '참나무 기둥', 'Rank' : '일반', 'Amount' : 1, 'Maker' : [
+            
+            {'Name' : '참나무', 'Rank' : '일반', 'Amount' : 3},
+            
+            {'Name' : '자작나무', 'Rank' : '일반', 'Amount' : 1},
+            
+            {'Name' : '돌', 'Rank' : '일반', 'Amount' : 1}
+        
+            ]
+        },
+
+        {'Name' : '참나무 판자', 'Rank' : '일반', 'Amount' : 4, 'Maker' : [
+            
+            {'Name' : '참나무', 'Rank' : '일반', 'Amount' : 2},
+            
+            {'Name' : '소나무', 'Rank' : '일반', 'Amount' : 2}
+        
+            ]
+        }
+    
+    ],
+
+    '2' : [
+
+        {'Name' : '주괴 거푸집', 'Rank' : '일반', 'Amount' : 1, 'Maker' : [
+            
+            {'Name' : '돌', 'Rank' : '일반', 'Amount' : 5},
+            
+            {'Name' : '구리', 'Rank' : '일반', 'Amount' : 1},
+
+            {'Name' : '철', 'Rank' : '희귀', 'Amount' : 2}
+        
+            ]
+        },
+
+        {'Name' : '수액 적출기', 'Rank' : '희귀', 'Amount' : 1, 'Maker' : [
+            
+            {'Name' : '자작나무', 'Rank' : '일반', 'Amount' : 3},
+            
+            {'Name' : '가문비나무', 'Rank' : '일반', 'Amount' : 3}
+        
+            ]
+        },
+
+        {'Name' : '화로', 'Rank' : '초희귀', 'Amount' : 1, 'Maker' : [
+            
+            {'Name' : '자작나무', 'Rank' : '일반', 'Amount' : 5},
+            
+            {'Name' : '돌', 'Rank' : '일반', 'Amount' : 9},
+
+            {'Name' : '철', 'Rank' : '일반', 'Amount' : 12}
+        
+            ]
+        }
+
+    ],
+
+    '3' : [
+
+        {'Name' : '철괴', 'Rank' : '희귀', 'Amount' : 1, 'Maker' : [
+            
+            {'Name' : '철', 'Rank' : '일반', 'Amount' : 3},
+            
+            {'Name' : '화로', 'Rank' : '초희귀', 'Amount' : 0},
+
+            {'Name' : '주괴 거푸집', 'Rank' : '일반', 'Amount' : 1}
+        
+            ]
+        },
+
+        {'Name' : '구리 주괴', 'Rank' : '희귀', 'Amount' : 1, 'Maker' : [
+            
+            {'Name' : '구리', 'Rank' : '일반', 'Amount' : 3},
+            
+            {'Name' : '화로', 'Rank' : '초희귀', 'Amount' : 0},
+
+            {'Name' : '주괴 거푸집', 'Rank' : '일반', 'Amount' : 1}
+        
+            ]
+        }
+
+    ],
+
+    '4' : [
+
+        {'Name' : '나사 거푸집', 'Rank' : '희귀', 'Amount' : 1, 'Maker' : [
+            
+            {'Name' : '돌', 'Rank' : '일반', 'Amount' : 5},
+            
+            {'Name' : '철괴', 'Rank' : '희귀', 'Amount' : 3}
+        
+            ]
+        },
+
+        {'Name' : '판 거푸집', 'Rank' : '희귀', 'Amount' : 1, 'Maker' : [
+            
+            {'Name' : '돌', 'Rank' : '일반', 'Amount' : 5},
+            
+            {'Name' : '철괴', 'Rank' : '희귀', 'Amount' : 3}
+        
+            ]
+        },
+
+        {'Name' : '철근', 'Rank' : '희귀', 'Amount' : 1, 'Maker' : [
+            
+            {'Name' : '돌', 'Rank' : '일반', 'Amount' : 2},
+            
+            {'Name' : '철괴', 'Rank' : '일반', 'Amount' : 6}
+        
+            ]
+        }
+
+    ],
+
+    '5' : [
+
+        {'Name' : '구리 나사', 'Rank' : '초희귀', 'Amount' : 2, 'Maker' : [
+            
+            {'Name' : '구리 주괴', 'Rank' : '희귀', 'Amount' : 3},
+
+            {'Name' : '나사 거푸집', 'Rank' : '희귀', 'Amount' : 1}
+        
+            ]
+        },
+
+        {'Name' : '구리 판', 'Rank' : '초희귀', 'Amount' : 1, 'Maker' : [
+            
+            {'Name' : '구리 주괴', 'Rank' : '희귀', 'Amount' : 3},
+            
+            {'Name' : '판 거푸집', 'Rank' : '희귀', 'Amount' : 1}
+        
+            ]
+        },
+
+        {'Name' : '철 나사', 'Rank' : '초희귀', 'Amount' : 2, 'Maker' : [
+            
+            {'Name' : '철괴', 'Rank' : '희귀', 'Amount' : 3},
+
+            {'Name' : '나사 거푸집', 'Rank' : '희귀', 'Amount' : 1}
+        
+            ]
+        },
+
+        {'Name' : '철판', 'Rank' : '초희귀', 'Amount' : 1, 'Maker' : [
+            
+            {'Name' : '철괴', 'Rank' : '희귀', 'Amount' : 3},
+            
+            {'Name' : '판 거푸집', 'Rank' : '희귀', 'Amount' : 1}
+        
+            ]
+        }
+
+    ],
+
+    '6' : [],
+
+    '7' : [],
+
+    '8' : [],
+
+    '9' : [],
+
+    '10' : []
+}
